cd750e7a62342aeac15c50953f4f097eba0e1f87
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-06-21 11:15:02 -0400

    collected sttic files

--- restclient/restclient/settings.py
+++ restclient/restclient/settings.py
@@ -120,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = str(BASE_DIR.joinpath('static'))
 STATICFILES_FINDERS = [
     # First add the two default Finders, since this will overwrite the default.
     "django.contrib.staticfiles.finders.FileSystemFinder",
